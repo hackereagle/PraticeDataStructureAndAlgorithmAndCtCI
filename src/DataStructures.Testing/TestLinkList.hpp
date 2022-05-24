36eeb2b460a3e43ab97141d604dc5a1921876e4c
sc.lin <read0651@hotmail.com>
2022-05-24 22:22:22 +0800

    Chg: Added PushBack, PushFront, PopBack and PopFront method. All refer to c++ std::list.

---
+++ src/DataStructures.Testing/TestLinkList.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include "StringBuilder.hpp"
+#include "AssertClass.hpp"
+
+class TestLinkList
+{};
--- src/DataStructures/LinkList.hpp
+++ src/DataStructures/LinkList.hpp
@@ -1,16 +1,109 @@
 #pragma once
+#include <iostream>
 
 template<class T>
 class Node
 {
 public:
 	Node(T data)
-	{}
+	{
+		this->Data = data;
+		this->Previous = nullptr;
+		this->Next = nullptr;
+	}
+
+	Node(T data, Node<T>* pre)
+	{
+		this->Data = data;
+		this->Previous = pre;
+		this->Next = nullptr;
+	}
+
+	Node(T data, Node<T>* pre, Node<T>* next)
+	{
+		this->Data = data;
+		this->Previous = pre;
+		this->Next = next;
+	}
 
 	~Node()
 	{}
 
+	T Data;
+	Node<T>* Next;
+	Node<T>* Previous;
+};
+
+template<class T>
+class LinkList
+{
+public:
+	LinkList()
+	{
+		this->mHead = nullptr;
+		this->mTail = nullptr;
+	}
+
+	~LinkList()
+	{
+		if(this->mHead){
+			Node<T>* temp;
+			while(!this->mHead->Next){
+				temp = this->mHead->Next;
+				delete this->mHead;
+				this->mHead = temp;
+			}
+			delete this->mHead;
+
+			this->mHead = nullptr;
+			this->mTail = nullptr;
+		}
+	}
+
+
+	void PushBack(T data)
+	{
+		if(!this->mHead){
+			this->mHead = new Node<T>(data);
+			this->mTail = this->mHead;
+		}
+		else{
+			this->mTail->Next = new Node<T>(data);
+			this->mTail = this->mTail->Next;
+		}
+	}
+
+	void PushFront(T data)
+	{
+		this->mHead = new Node<T>(data, this->mHead);
+	}
+
+	void PopBack(T data)
+	{
+		T ret;
+		ret = this->mTail->Data;
+	}
+
+	void PopFront(T data)
+	{
+
+	}
+
+	bool IsEmpty()
+	{}
+
+	void Reverse()
+	{}
+
+	T operator[](unsigned int index)
+	{}
+
+	size_t Length() { return this->mLength; }
+
+
 private:
-	T mData;
-	Node<T>* mNext;
+	Node<T>* mHead;
+	Node<T>* mTail;
+	Node<T>* mCurrent;
+	size_t mLength;
 };
