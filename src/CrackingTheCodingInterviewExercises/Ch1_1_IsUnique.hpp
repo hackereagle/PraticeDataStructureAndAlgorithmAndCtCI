1f001b370436a7ea7b93f76f66095741b681c99c
sc.lin <read0651@hotmail.com>
2022-06-07 16:44:28 +0800

    New: Finished CtCI CH1 question 1. Is Unique.

--- src/CrackingTheCodingInterviewExercises/Ch1_1_IsUnique.hpp
+++ src/CrackingTheCodingInterviewExercises/Ch1_1_IsUnique.hpp
@@ -1,7 +1,8 @@
 #pragma once
+#include <string>
 #include "TestClassBase.hpp"
 
-class Ch1_1_IsUnique: public TestClassBase
+class Ch1_1_IsUnique
 {
 public:
 	Ch1_1_IsUnique()
@@ -9,4 +10,75 @@ public:
 
 	~Ch1_1_IsUnique()
 	{}
+
+	bool Solution(std::string str) 
+	{
+		bool isUnique = false;
+
+		long long int flags = 0;
+		size_t len = str.size();
+		for(int i = 0; i < len; i++){
+			int index = (int)(str[i] - 'A');
+			if(true == GetBit(flags, index)){
+				isUnique = false;
+				break;
+			}
+			else{
+				flags = SetBit(flags, index);
+				isUnique = true;
+			}
+		}
+
+		return isUnique;
+	}
+
+private:
+
+	bool GetBit(long long int num, int bit)
+	{
+		return num & ((long long int)1 << bit);
+	}
+
+	long long int SetBit(long long int num, int bit)
+	{
+		return num | ((long long int)1 << bit);
+	}
+};
+
+class Ch1_1_IsUnique_Test: public TestClassBase
+{
+public:
+	Ch1_1_IsUnique_Test()
+	{}
+
+	~Ch1_1_IsUnique_Test()
+	{}
+
+	void Test() override
+	{
+		Test_UniqueStr_abcdefghABCDE();
+
+		Test_NotUniqueStr_abcbadeffgh();
+	}
+
+private:
+	Ch1_1_IsUnique mAlgorithm;
+
+	void Test_UniqueStr_abcdefghABCDE()
+	{
+		PrintfTestTitle("Test unique string \"abcdefghABCDE\"");
+		std::string input = "abcdefghABCDE";
+
+		bool result = mAlgorithm.Solution(input);
+		Assert(result);
+	}
+
+	void Test_NotUniqueStr_abcbadeffgh()
+	{
+		PrintfTestTitle("Test unique string \"abcbadeffgh\"");
+		std::string input = "abcbadeffgh";
+
+		bool result = mAlgorithm.Solution(input);
+		Assert(!result);
+	}
 };
--- src/CrackingTheCodingInterviewExercises/main.cpp
+++ src/CrackingTheCodingInterviewExercises/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include "TestClassBase.hpp"
+#include "Ch1_1_IsUnique.hpp"
 
 int main(int argc, char** argv)
 {
+	std::vector<std::unique_ptr<TestClassBase>> tests;
+	tests.push_back(std::make_unique<Ch1_1_IsUnique_Test>());
+
+	for(std::unique_ptr<TestClassBase>& t : tests){
+		t->Test();
+	}
+
 	return EXIT_SUCCESS;
 }
