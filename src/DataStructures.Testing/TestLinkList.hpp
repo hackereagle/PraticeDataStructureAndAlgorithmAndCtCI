d3e9e47728b8b7bc6e90e5ac7d7d0bedf6728395
Bill <read0651@hotmail.com>
2022-06-15 14:58:22 +0800

    Chg: Wrote testing PushBack method.

--- src/DataStructures.Testing/TestLinkList.hpp
+++ src/DataStructures.Testing/TestLinkList.hpp
@@ -4,17 +4,30 @@
 #include "AssertClass.hpp"
 #include "LinkList.hpp"
 
+#define TEST_DATA_LEN (10)
+
 class TestLinkList
 {
 public:
 	TestLinkList()
-	{}
+	{
+		InitializeTestData(this->mTestData, TEST_DATA_LEN);
+	}
 
 	~TestLinkList()
 	{}
 
 	void TestPushBack()
 	{
+		LinkList<int> linkList;
+		
+		for(int i = 0; i < 5; i++)
+			linkList.PushBack(this->mTestData[i]);
+
+		AssertClass::GetInstance().Assert(linkList.IsEmpty() == false, "Link list has element");
+		AssertClass::GetInstance().Assert(linkList.Length() == 5, "Link list length");
+		for(int i = 0; i < 5; i++)
+			AssertClass::GetInstance().Assert(linkList[i] == this->mTestData[i]);
 	}
 
 	void TestPushFront()
@@ -29,5 +42,15 @@ public:
 	void TestPopFront()
 	{
 
+	}
+
+private:
+	int mTestData[TEST_DATA_LEN];
+
+	void InitializeTestData(int *arr, int count)
+	{
+		for(int i = 0; i < count; i++){
+			*(arr + i) = i + 5;
+		}
 	}
 };
